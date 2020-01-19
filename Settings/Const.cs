70b89df8d8a520e2d2ab97b33808cfd29bb3ad1e
levonij <levonij@yandex.ru>
2020-01-19 17:42:30 +0300

    фикс

--- Settings/Const.cs
+++ Settings/Const.cs
@@ -9,13 +9,17 @@ namespace CRMBytholod.Settings
     {
         //local
         //public const string ConnectionString = @"Data Source = DENIS-ПК; Initial Catalog = Bytholod; Integrated Security = True";
-        public const string ConnectionString = @"Data Source=DENIS-ПК; Initial Catalog=Bytholod;User ID=holodec;Password=9faw8@f2GA2";
+        // MAIN
+        //public const string ConnectionString = @"Data Source=DENIS-ПК; Initial Catalog=Bytholod;User ID=holodec;Password=9faw8@f2GA2";
 
         //удаленная БД
         //public const string ConnectionString = @"Data Source=tcp:crm.bytholod.com,1433;Initial Catalog=Bytholod;User ID=SA;Password=7R7r5L2g";
         //public const string ConnectionString = @"Data Source=tcp:crm.bytholod.com,1433;Initial Catalog=Bytholod;User ID=SA;Password=7R7r5L2g";
-        
-        //public const string ConnectionString = @"Data Source=crm.bytholod.com; Initial Catalog=Bytholod;User ID=SA;Password=7R7r5L2g Integrated Security = True";
+
+
+        //public const string ConnectionString = @"Data Source = crm.bytholod.com; Initial Catalog = Bytholod; Integrated Security = True";
+
+        public const string ConnectionString = @"Data Source=crm.bytholod.com; Initial Catalog=Bytholod;User ID=SA;Password=7R7r5L2g";
 
     }
 }
